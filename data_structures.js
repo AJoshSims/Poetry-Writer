542722294312d42d593fb22fec0c0cbbcbf67ffe
Joshua Sims <JoshuaSimsVA@gmail.com>
2016-10-03 04:47:34 -0400

    different function name

--- data_structures.js
+++ data_structures.js
@@ -5,128 +5,123 @@ var fs = require("fs");
 
 /**
  *
- * @type {number}
  */
-var words = 0;
+var inputFileName = process.argv[2];
 
 /**
  *
- * @type {{}}
  */
 var wordCount = {};
 
 /**
  *
- * @type {{}}
  */
 var wordFreq = {};
 
 /**
  *
- * @type {{}}
  */
 var condWordCount = {};
 
 /**
  *
- * @type {{}}
  */
 var condWordFreq = {};
+{
 
-// /**
-//  *
-//  */
-// function wordCount(word)
-// {
-//     wordCount[word]++;
-// }
+}
+
+//
+main(inputFileName);
 
 /**
- * Reads the input file and
+ *
+ * @param inputFileName
  */
-function readFile()
+function readInputFile(inputFileName)
 {
-    var fileInName = process.argv[2];
-
     console.log("Reading file...");
 
     // Opens the input file for reading and reads its entirety while
     // storing its contents, as a string, into fileInContent.
-    var fileInContent = fs.readFileSync(fileInName, "utf8");
+    var fileInContent = fs.readFileSync(inputFileName, "utf8");
 
+    return fileInContent;
+}
+
+/**
+ *
+ * @param inputFileContent
+ */
+function parseInputFile(inputFileContent)
+{
     console.log("Parsing data...");
+    //
+    var inputFileWords = inputFileContent.trim().split(/\s+/);
 
     //
-    var firstWord = null;
-
-    //
-    var previousWord = null;
-
-    var fileInContentArray = fileInContent.trim().split(/\s+/);
-
-    if (fileInContentArray.length == 1
-        && fileInContentArray[fileInContentArray.length - 1] == "")
+    if (inputFileWords.length == 1
+        && inputFileWords[inputFileWords.length - 1] == "")
     {
         console.log("Input can not be empty or only be whitespace.");
         process.exit(1);
     }
 
     //
-    fileInContent.trim().split(/\s+/).forEach(
-        function(currentWord)
-        {
-            //TODO remove
-            console.log(currentWord);
+    return inputFileWords;
+}
 
-            words++;
+/**
+ *
+ */
+function wordCountFunc(currentWord)
+{
+    if (wordCount[currentWord] == undefined)
+    {
+        wordCount[currentWord] = 1;
 
-            if (wordCount[currentWord] == undefined)
-            {
-                wordCount[currentWord] = 1;
+        condWordCount[currentWord] = {};
 
-                condWordCount[currentWord] = {};
-            }
+        condWordFreq[currentWord] = {};
+    }
 
-            else
-            {
-                wordCount[currentWord] = wordCount[currentWord] + 1;
-            }
+    else
+    {
+        wordCount[currentWord] = wordCount[currentWord] + 1;
+    }
 
-            // TODO remove
-            console.log(currentWord + " occurs " +
-                wordCount[currentWord] + " times.");
+    // TODO remove
+    console.log(currentWord + " occurs " +
+        wordCount[currentWord] + " times.");
+}
 
-            if (previousWord == null)
-            {
-                firstWord = currentWord;
-            }
+/**
+ *
+ */
+function condWordCountFunc(previousWord, currentWord)
+{
+    if (condWordCount[previousWord][currentWord] == undefined)
+    {
+        condWordCount[previousWord][currentWord] = 1;
+    }
 
-            else
-            {
-                if (condWordCount[previousWord][currentWord] == undefined)
-                {
-                    condWordCount[previousWord][currentWord] = 1;
-                }
+    else
+    {
+        condWordCount[previousWord][currentWord] =
+            condWordCount[previousWord][currentWord] + 1;
+    }
 
-                else
-                {
-                    condWordCount[previousWord][currentWord] =
-                        condWordCount[previousWord][currentWord] + 1;
-                }
+    // TODO remove
+    console.log(currentWord + " occurs " +
+        condWordCount[previousWord][currentWord] + " times after " +
+        previousWord);
+}
 
-                // TODO remove
-                console.log(currentWord + " occurs " +
-                    condWordCount[previousWord][currentWord] + " times after " +
-                    previousWord);
-            }
-
-            previousWord = currentWord;
-        }
-    );
-
-    var lastWord = previousWord;
-    previousWord = null;
-
+/**
+ *
+ */
+function condWordCountForLastWord(firstWord, lastWord)
+{
     if (condWordCount[lastWord][firstWord] == undefined)
     {
         condWordCount[lastWord][firstWord] = 1;
@@ -142,21 +137,70 @@ function readFile()
     console.log(firstWord + " occurs " +
         condWordCount[lastWord][firstWord] + " times after " +
         lastWord);
+}
 
-    condWordFreq[lastWord] =
-        {firstWord : condWordCount[lastWord][firstWord] / words};
+/**
+ *
+ */
+function calculateWordCounts(inputFileWords)
+{
+    //
+    var words = 0;
 
+    //
+    var firstWord = null;
+    //
+    var previousWord = null;
+    //
+    inputFileWords.forEach(
+        function(currentWord)
+        {
+            words++;
+
+            wordCountFunc(currentWord);
+
+            if (previousWord == null)
+            {
+                firstWord = currentWord;
+            }
+
+            else
+            {
+                condWordCountFunc(previousWord, currentWord);
+            }
+
+            previousWord = currentWord;
+        }
+    );
+
+    //
+    var lastWord = previousWord;
+    condWordCountForLastWord(firstWord, lastWord);
+
+    return words;
+}
+
+/**
+ *
+ */
+function wordFreqFunc(words)
+{
     for (var word in wordCount)
     {
-            wordFreq[word] = wordCount[word] / words;
-
-            condWordFreq[word] = {};
+        wordFreq[word] = wordCount[word] / words;
 
         //TODO remove
         console.log("There is a " + wordFreq[word] + " chance of " +
             "encountering " + word);
     }
 
+}
+
+/**
+ *
+ */
+function condWordFreqFunc(words)
+{
     for (var wordBefore in condWordCount)
     {
         for (var wordAfter in condWordCount[wordBefore])
@@ -164,7 +208,7 @@ function readFile()
             condWordFreq[wordBefore][wordAfter] =
                 condWordCount[wordBefore][wordAfter] / words;
 
-            //TODO remove
+            // TODO remove
             console.log("There is a " + condWordFreq[wordBefore][wordAfter] +
                 " chance of " + "encountering " + wordAfter + " after " +
                 wordBefore);
@@ -172,9 +216,30 @@ function readFile()
     }
 }
 
-readFile();
+/**
+ *
+ */
+function calculateWordFrequencies(words)
+{
+    wordFreqFunc(words);
 
-//TODO remove
-console.log("The number of words in that file is " + words + ".");
+    condWordFreqFunc(words);
+}
 
+/**
+ *
+ */
+function printWordCountsAndWordFrequencies()
+{
+
+}
+
+function main(inputFileName)
+{
+    var inputFileWords = parseInputFile(readInputFile(inputFileName));
+
+    calculateWordFrequencies(calculateWordCounts(inputFileWords));
+
+    printWordCountsAndWordFrequencies();
+}
 
