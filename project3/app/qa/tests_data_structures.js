21271428e531baaac0ae3cddeebd33f741de0275
Joshua Sims <JoshuaSimsVA@gmail.com>
2016-11-13 08:16:54 -0500

    omitting-excessive-error-handling

--- project3/app/qa/tests_data_structures.js
+++ project3/app/qa/tests_data_structures.js
@@ -7,21 +7,11 @@ suite("Tests for data_structures.js", function()
 	const dataStructuresFile = require("../data_structures");
 	const errors = require("../errors");
 
-	// TODO not done
-	test("readInputFile test", function()
+	test("readInputFile", function()
 	{
 		const readInputFile = dataStructuresFile.readInputFile;
-
 		var inputFileContent = null;
-		try
-		{
-			inputFileContent = readInputFile("non-existent-file");
-		}
-		catch (error)
-		{
-			assert.deepStrictEqual(
-				error instanceof errors.InputFileDoesNotExistError, false);
-		}
+
 		try
 		{
 			inputFileContent = readInputFile(3);
@@ -29,12 +19,52 @@ suite("Tests for data_structures.js", function()
 		catch (error)
 		{
 			assert.deepStrictEqual(
-				error instanceof errors.InputFilePathIsNotStringError, false);
+				error instanceof errors.InputFilePathIsNotStringError, true);
 		}
+
+		try
+		{
+			inputFileContent = readInputFile("non-existent-file");
+		}
+		catch (error)
+		{
+			assert.deepStrictEqual(
+				error instanceof errors.InputFileDoesNotExistError, true);
+		}
+
+		inputFileContent = readInputFile("rbbrrg_input_text.txt");
 	});
 
 	test("parseInputFile", function()
 	{
+		const parseInputFile = dataStructuresFile.parseInputFile;
+		var inputFileWords = null;
 
+		inputFileWords = parseInputFile("this is four words");
+		assert.deepStrictEqual(inputFileWords.length === 4, true);
+
+		try
+		{
+			inputFileWords = parseInputFile("");
+		}
+		catch (error)
+		{
+			assert.deepStrictEqual(
+				error instanceof errors.
+				InputCannotBeEmptyOrOnlyWhitespaceError,
+				true);
+		}
+
+		try
+		{
+			inputFileWords = parseInputFile("    \t       \n    ");
+		}
+		catch (error)
+		{
+			assert.deepStrictEqual(
+				error instanceof errors.
+				InputCannotBeEmptyOrOnlyWhitespaceError,
+				true);
+		}
 	});
 });
